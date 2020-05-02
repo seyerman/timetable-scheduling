f10ec7a32ee8b03b03dd89b9d86b871c6c06e0b8
Juan Manuel Reyes Garcia <seyerman@gmail.com>
2020-05-02 18:30:32 -0500

    remove import innecesary

--- resource-allocation/src/ui/Main.java
+++ resource-allocation/src/ui/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
